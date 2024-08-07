838f56acf11556e2302f34cb01bede6e991df06d
Ivan Križaj <ivan.krizaj@chanalyze.com>
2024-08-07 12:02:43 +0200

    add

--- src/server.js
+++ src/server.js
@@ -11,13 +11,13 @@ const convertImage = require("../examples/convertEmfToSvg");
 
 const port = 8080;
 
-// Configure multer for file uploads
-const upload = multer({ dest: path.join(__dirname, 'uploads') });
+// // Configure multer for file uploads
+// const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/output', express.static(path.join(__dirname, '../examples/_output')));
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use('/output', express.static(path.join(__dirname, '../examples/_output')));
 
 
 app.get("/test", (req, res) => res.send("Express on Vercel"));
