e5cf31a8b7201ddba34aee1d4df52121fc0eac08
Ivan Križaj <ivan.krizaj@chanalyze.com>
2024-08-07 12:09:30 +0200

    add

--- src/server.js
+++ src/server.js
@@ -11,21 +11,25 @@ const convertImage = require("../examples/convertEmfToSvg");
 
 const port = 8080;
 
-// // Configure multer for file uploads
+// Configure multer for file uploads
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use('/output', express.static(path.join(__dirname, '../examples/_output')));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/output', express.static(path.join(__dirname, '../examples/_output')));
 
-app.get("/", (req, res) => res.send("Express on Vercel1"));
+const cleanupMiddleware = (req, res, next) => {
+  cleanupFolder('../examples/_output');
+  cleanupFolder('uploads');
+  next();
+};
 
 
-app.get("/test", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 // Define a route handler for file upload
-app.post('/upload', upload.single('file'), async (req, res) => {
+app.post('/upload',cleanupMiddleware, upload.single('file'), async (req, res) => {
   if (!req.file) {
     return res.status(400).json({ error: 'No file uploaded' });
   }
@@ -61,6 +65,33 @@ app.post('/upload', upload.single('file'), async (req, res) => {
   }
 });
 
+// Function to clean up files in the exports folder
+function cleanupFolder(folderName) {
+  const dirPath = path.join(__dirname, folderName);
+  fs.readdir(dirPath, (err, files) => {
+    if (err) {
+      console.error(`Error reading ${folderName} directory:`, err);
+      return;
+    }
+
+    files.forEach(file => {
+      const filePath = path.join(dirPath, file);
+      fs.unlink(filePath, err => {
+        if (err) {
+          console.error('Error deleting:', err);
+        } else {
+          console.log(`Deleted ${folderName}: ${filePath}`);
+        }
+      });
+    });
+  });
+}
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 // Start the server
 app.listen(port, () => {
