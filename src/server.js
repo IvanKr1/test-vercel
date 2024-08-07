c1c35e36bac2a7791b51eacda7c61f16fc45db77
Ivan Križaj <ivan.krizaj@chanalyze.com>
2024-08-07 11:42:28 +0200

    make a src folder

--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
+app.get("/test", (req, res) => res.send("Express on Vercel"));
+
 app.get('/about', (req, res) => {
   res.send('About route 🎉 ')
 })
